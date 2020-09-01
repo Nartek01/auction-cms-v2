3ce78526c11699dad3df325fc92e39c853ddf8c9
nartek01 <nartek01@gmail.com>
2020-09-01 21:10:03 +0200

    First commit

---
+++ src/components/Invoices.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="invoices">
+    <h1>Invoices here!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+name: 'Invoices',
+data() {
+  return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Settings.vue
@@ -0,0 +1,18 @@
+<template>
+<section id="settings">
+  <h1>Settings options here!</h1>
+  </section>  
+</template>
+
+<script>
+export default {
+name: 'Settings',
+  data() {
+   return {} 
+  }
+}
+</script>
+
+<style>
+
+</style>
