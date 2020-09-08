23176a92ebd4f9278c85869c22857e9c8f28cd0f
Master Nara <nartek01@gmail.com>
2020-09-08 10:45:54 +0200

    Removed variable for conn.query

--- backend/express_server/index.js
+++ backend/express_server/index.js
@@ -19,7 +19,6 @@ var app = express()
 const server = require('http').createServer(app)
 
 app.use(cors({credentials: true, origin: true}), express.json(), fileUpload(), bodyParser.json(),bodyParser.urlencoded({extended: true}))
-
 //Connecting to database with error catching
 conn.connect((req,res,err) => {
   if(err) {
@@ -38,19 +37,16 @@ server.listen(port, () => {
 
 app.get('/', function (req, res) {
   conn.query('SELECT * FROM products', function (error, results) {
-      if (error, console.log('There is an issue')) throw error; 
-      return res.send({ error: false, data: results, message: 'The data is send anyhow.' });
+    if (error, console.log('There is an issue')) throw error; 
+    return res.send({ error: false, data: results, message: 'The data is send anyhow.' });
   });
-});
+})
 
 app.post('/products',(req, res) => {
   let data = {product_name: req.body.product_name, description: req.body.description, price: req.body.price};
   let sql = "INSERT INTO products SET ?";
-  let query = conn.query(sql, data,(error, results) => {
+  conn.query(sql, data,(error, results) => {
     if(error) throw error;
     res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
   });
 });
-
-
-
