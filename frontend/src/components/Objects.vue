cac5dd94242620765de9c65fefdb75baf6a5070e
MonikaMsk <monika.musak@gmail.com>
2020-09-10 12:54:17 +0200

    Modified object component

--- frontend/src/components/Objects.vue
+++ frontend/src/components/Objects.vue
@@ -1,24 +1,29 @@
 <template>
   <section id="Additem">
-    <h1>Add a new object</h1>
+    <h1>Add an item</h1>
     <form action="POST">
       <p class="h4 text-center mb-4"></p>
       <div class="grey-text">
-
+        
         <mdb-input v-model="name" label="Object name" group type="text" validate error="wrong" success="right" />
-        <mdb-textarea v-model="description" :row="2" label="Object description" /></div>
-         <mdb-input v-model="category" label="Object category" group type="text" validate error="wrong" success="right" />
-        <mdb-input v-model ="status" label="Object status" group type="text" validate error="wrong" success="right" />
-        <mdb-input v-model="startPrice"  label="Start price" group type="number" validate error="wrong" success="right"/>
-        <mdb-input v-model="personalNumber" label="Seller (personal number to be entered)" group type="number" validate error="wrong" success="right" />
+        <mdb-textarea v-model="description" :row="2" label="Object description" />
+      
+      <mdb-input v-model="category" label="Category" group type="text" validate error="wrong" success="right" />
+      <mdb-input v-model="status" label="Status" group type="text" validate error="wrong" success="right" />
+      <mdb-input v-model="personalNumber" label="Seller (personal number to be entered)" group type="number" validate error="wrong" success="right"/>
+        <mdb-input v-model="startPrice" label="Start price" group type="number"  validate error="wrong" success="right"/>
         <mdb-input v-model="reservePrice" label="Reserve price" group type="number" validate error="wrong" success="right" />
         <mdb-input v-model="currency" label="Currency" group type="text" validate error="wrong" success="right" />
-        <mdb-input v-model="date" label="Date" group type="date" validate error="wrong" success="right" />
-      <!--  <mdb-input v-model="photo" label="" enctype="multipart/form-data" group type="file" name="someExpressFile" @change.native="onFileSelected" accept="image/*" validate error="wrong" success="right"/> -->
+         <mdb-input label="Add image" ref="file" enctype="multipart/form-data" group type="file" name="photo"  @change="onFileChanged" accept="image/*" validate error="wrong" success="right" /> 
+        <mdb-input v-model="date" label="Date" group type="text" validate error="wrong" success="right" />
+       </div>
 
       <div class="text-center">
         <mdb-btn @click="addProduct()" outline="secondary">
           Add
+        </mdb-btn>
+         <mdb-btn @click="imageUpload()" outline="secondary">
+          Submit
         </mdb-btn>
       </div>
     </form>
@@ -26,7 +31,7 @@
 </template>
 
 <script>
-//import axios from 'axios'
+import axios from 'axios'
 import { mdbInput, mdbBtn, mdbTextarea } from "mdbvue";
 
 export default {
@@ -47,41 +52,60 @@ export default {
         startPrice: 0,
         reservePrice: 0,
         currency: "",
-       date:"" ,
-      //  photo: null, 
-     
+        date: "",
+        photo: "",
+        
     };
   },
-//file uploading
-  methods: {
-    onFileSelected(event) {
-      this.photo = event.target.files[0];
-    },
 
+  methods: {
+
+       
+  
    async addProduct() {
-                if (this.name) {
+                if (this.name && this.description && this.category && this.status && this.personalNumber && this.startPrice &&this.reservePrice && this.currency ) {
                    
                     const response = await fetch("http://localhost:3000/products", {
                         "method": "POST",
                         "headers": {
-                            "content-type": "application/json",
-                             "Access-Control-Allow-Origin": "*",
+                            "content-type": "application/json"
                         },
                         "body":
-                            JSON.stringify({product_name: this.name, description: this.description,category: this.category, product_status: this.status,personal_number: this.personalNumber,
-                            start_price: this.startPrice,reserve_price: this.reservePrice, currency: this.currency, date_added: this.date })
+                            JSON.stringify({product_name: this.name, description: this.description, category: this.category, product_status: this.status,
+                            personal_number: this.personalNumber, start_price: this.startPrice, reserve_price: this.reservePrice, currency: this.currency,date_added: this.date})
                     })
                     const data = await response.json()
                     if (data.status == '200') {
-                        alert('New object has been added')
+                        alert('All is good')
                     } else {
                         alert('Something went wrong!')
                     }
 
                 } else {
-                    alert('Please enter the name of your object')
+                    alert('Please fill in the form')
                 }
+            },
+
+//Image uploading:
+
+            onFileChange(event){
+              let selectedFile = event.target.files[0]
+         this.photo = selectedFile
+         
+       },
+
+          async imageUpload(){
+            const formData = new FormData();
+            formData.append('photo', this.photo);
+            try{
+              await axios.post('/upload',formData)
+              alert('Success')
             }
+            catch(err){
+              alert(err)
+            }
+          }
+
 }
 }
 
@@ -97,4 +121,3 @@ export default {
   margin-bottom: 0;
 }
 </style>
-
